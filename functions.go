eb9c23ff6b91d2698c5c7962a6ac086183e758ad
Aleksandr Voronkov <avoronkov@plesk.com>
2020-04-29 17:40:42 +0700

    add more operations, variables support

--- functions.go
+++ functions.go
@@ -7,6 +7,11 @@ type Func func([]Expr) (Expr, error)
 var Funcs = map[string]Func{
 	"print": FPrint,
 	"+":     FPlus,
+	"-":     FMinus,
+	"*":     FMultiply,
+	"<":     FLess,
+	"=":     FEq,
+	"not":   FNot,
 }
 
 func FPrint(args []Expr) (Expr, error) {
@@ -30,4 +35,97 @@ func FPlus(args []Expr) (Expr, error) {
 		result += int(a)
 	}
 	return Int(result), nil
+}
+
+func FMinus(args []Expr) (Expr, error) {
+	var result int
+	for i, arg := range args {
+		a, ok := arg.(Int)
+		if !ok {
+			return nil, fmt.Errorf("FMinus: expected integer argument, found %v", arg.Repr())
+		}
+		if i == 0 {
+			result = int(a)
+		} else {
+			result -= int(a)
+		}
+	}
+	return Int(result), nil
+}
+
+func FMultiply(args []Expr) (Expr, error) {
+	var result int = 1
+	for _, arg := range args {
+		a, ok := arg.(Int)
+		if !ok {
+			return nil, fmt.Errorf("FMultiply: expected integer argument, found %v", arg.Repr())
+		}
+		result *= int(a)
+	}
+	return Int(result), nil
+}
+
+func FLess(args []Expr) (Expr, error) {
+	if len(args) != 2 {
+		return nil, fmt.Errorf("FLess: expected 2 arguments, found %v", args)
+	}
+	a, ok := args[0].(Int)
+	if !ok {
+		return nil, fmt.Errorf("FLess: first argument should be integer, found %v", args[0].Repr())
+	}
+	b, ok := args[1].(Int)
+	if !ok {
+		return nil, fmt.Errorf("FLess: second argument should be integer, found %v", args[1].Repr())
+	}
+	return Bool(a < b), nil
+}
+
+func FEq(args []Expr) (Expr, error) {
+	if len(args) != 2 {
+		return nil, fmt.Errorf("FEq: expected 2 arguments, found %v", args)
+	}
+	switch a := args[0].(type) {
+	case Int:
+		b, ok := args[1].(Int)
+		if !ok {
+			return nil, fmt.Errorf("FEq: Expected second argument to be Int, found %v", args[1].Repr())
+		}
+		return Bool(a == b), nil
+	case Str:
+		b, ok := args[1].(Str)
+		if !ok {
+			return nil, fmt.Errorf("FEq: Expected second argument to be Str, found %v", args[1].Repr())
+		}
+		return Bool(a == b), nil
+	case Ident:
+		b, ok := args[1].(Ident)
+		if !ok {
+			return nil, fmt.Errorf("FEq: Expected second argument to be Ident, found %v", args[1].Repr())
+		}
+		return Bool(a == b), nil
+	case Bool:
+		b, ok := args[1].(Bool)
+		if !ok {
+			return nil, fmt.Errorf("FEq: Expected second argument to be Bool, found %v", args[1].Repr())
+		}
+		return Bool(a == b), nil
+	case *Sexpr:
+		b, ok := args[1].(*Sexpr)
+		if !ok {
+			return nil, fmt.Errorf("FEq: Expected second argument to be Str, found %v", args[1].Repr())
+		}
+		return Bool(a.Repr() == b.Repr()), nil
+	}
+	panic(fmt.Errorf("Unknown argument type: %v (%T)", args[0], args[0]))
+}
+
+func FNot(args []Expr) (Expr, error) {
+	if len(args) != 1 {
+		return nil, fmt.Errorf("FNot: expected 1 argument, found %v", args)
+	}
+	a, ok := args[0].(Bool)
+	if !ok {
+		return nil, fmt.Errorf("FNot: expected argument to be Bool, found %v", args[0].Repr())
+	}
+	return Bool(!bool(a)), nil
 }
--- interpreter.go
+++ interpreter.go
@@ -7,11 +7,15 @@ import (
 
 type Interpret struct {
 	parser *Parser
+	vars   map[string]Expr
+	funcs  map[string]Func
 }
 
 func NewInterpreter(r io.Reader) *Interpret {
 	return &Interpret{
 		parser: NewParser(r),
+		vars:   make(map[string]Expr),
+		funcs:  make(map[string]Func),
 	}
 }
 
@@ -38,7 +42,10 @@ func (i *Interpret) evalExpr(e Expr) (Expr, error) {
 	case Bool:
 		return a, nil
 	case Ident:
-		return nil, fmt.Errorf("Unexpected identifier: %v", a)
+		if value, ok := i.vars[string(a)]; ok {
+			return value, nil
+		}
+		return nil, fmt.Errorf("Unknown identifier: %v", a)
 	case *Sexpr:
 		if a.Quoted {
 			return a, nil
@@ -46,11 +53,46 @@ func (i *Interpret) evalExpr(e Expr) (Expr, error) {
 		if a.Len() == 0 {
 			return nil, fmt.Errorf("Unexpected empty s-expression: %v", a)
 		}
+		head := a.Head()
+		if name, ok := head.(Ident); ok {
+			if name == "if" {
+				return i.evalIf(a.Tail())
+			}
+			if name == "set" {
+				if err := i.setVar(a.Tail()); err != nil {
+					return nil, err
+				}
+				return &Sexpr{Quoted: true}, nil
+			}
+		}
+
 		return i.evalFunc(a)
 	}
 	panic(fmt.Errorf("Unexpected Expr type: %v (%T)", e, e))
 }
 
+// (cond) (expr-if-true) (expr-if-false)
+func (i *Interpret) evalIf(se *Sexpr) (Expr, error) {
+	if len(se.List) != 3 {
+		return nil, fmt.Errorf("Expected 3 arguments to if, found: %v", se)
+	}
+	arg := se.List[0]
+	res, err := i.evalExpr(arg)
+	if err != nil {
+		return nil, err
+	}
+	boolRes, ok := res.(Bool)
+	if !ok {
+		return nil, fmt.Errorf("Argument %v should evaluate to boolean value, actual %v", arg, res)
+	}
+	if bool(boolRes) {
+		return i.evalExpr(se.List[1])
+	} else {
+		return i.evalExpr(se.List[2])
+	}
+}
+
+// (func-name) (args...)
 func (i *Interpret) evalFunc(se *Sexpr) (Expr, error) {
 	head := se.Head()
 	name, ok := head.(Ident)
@@ -73,4 +115,25 @@ func (i *Interpret) evalFunc(se *Sexpr) (Expr, error) {
 	}
 	result, err := fn(args)
 	return result, err
+}
+
+// (var-name) (value)
+func (i *Interpret) setVar(se *Sexpr) error {
+	if se.Len() != 2 {
+		return fmt.Errorf("set wants 2 argument, found %v", se.Repr())
+	}
+	name, ok := se.List[0].(Ident)
+	if !ok {
+		return fmt.Errorf("set expected identifier first, found %v", se.List[0].Repr())
+	}
+	value, err := i.evalExpr(se.List[1])
+	if err != nil {
+		return err
+	}
+	i.vars[string(name)] = value
+	return nil
+}
+
+func (i *Interpret) defineFunc(se *Sexpr) error {
+	return fmt.Errorf("Not implemented yet")
 }
